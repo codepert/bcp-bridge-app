bfbe15bc026ddc96eb6858f129aecbb240d659be
tranguixdev <noahsflood908@gmail.com>
2024-01-18 04:43:56 +0900

    connect wallet, visual swap.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -36,15 +36,15 @@ const NavButtons = () => {
         <button
           type="button"
           className=" bg-indigo-700  text-white px-5 py-2.5 me-2 mb-2 text-sm rounded-lg hover:bg-blue-900 "
-          onClick={openConnectModal}
+          onClick={() => openConnectModal}
         >
           Connect Wallet
         </button>
       ) : (
         <button
           type="button"
-          className=" bg-indigo-700  text-white px-5 py-2.5 me-2 mb-2 text-sm rounded-lg hover:bg-blue-900 "
-          onClick={disconnect}
+          className=" bg-indigo-700  text-white px-5 py-2.5 me-2 mb-2 text-sm rounded-lg"
+          onClick={() => disconnect}
         >
           Wallet
         </button>
