d442956a94a816f7fbd46ca6ecaddd7c8a8a5660
tranguixdev <noahsflood908@gmail.com>
2024-01-24 05:02:58 +0900

    wallect connect.

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,4 +1,7 @@
+import { useTranslations } from 'next-intl';
+
 import Header from "./Header";
+import { HeadStrings } from "@/src/utils/types";
 
 type Props = {
   children: React.ReactNode;
@@ -6,9 +9,18 @@ type Props = {
 };
 
 const Layout = ({ children, className = "" }: Props) => {
+
+  const t = useTranslations('LayoutPage');
+
+  const headStrings: HeadStrings = {
+    btn_history: t('Head.btn_history'),
+    btn_connect: t('Head.btn_connect'),
+    btn_disconnect: t('Head.btn_disconnect')
+  }
+
   return (
     <div className="">
-      <Header />
+      <Header headStrings={headStrings} />
       <main className={`${className}`}>{children}</main>
     </div>
   );
